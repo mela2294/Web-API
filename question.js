bb7b9e7589bcc4850f5bf70bb8e4bdf73cd666a6
mela2294 <115947835+mela2294@users.noreply.github.com>
2023-02-02 18:57:22 -0500

    first commit

---
+++ question.js
@@ -0,0 +1,28 @@
+var questions = [
+
+    {
+        question: "Commonly used data types do not include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "3"
+    }, 
+    {
+        question: "The condition in a if / else statement is enclosed with ________",
+        choices: ["qoutes", "curly brackets", "parenthesis", "square brackets"],
+        answer: "3"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ________",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "4"
+    },
+    {
+        question: "Strings values must be enclosed within ________ wghen being assigned to variables",
+        choices: ["commas", "curly brackets", "qoutes", "parenthesis"],
+        answer: "3"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "4"
+    },
+]
---
+++ script.js
@@ -0,0 +1,91 @@
+var question = 0;
+var seconds = 60;
+const timer = document.getElementById('timer');
+var timerInterval;
+var temporaryMessageTimeout;
+const quizHeader = document.getElementById('quiz-header');
+const questions = document.getElementById('questions');
+
+//Quiz questions
+function setQuestion() {
+    quizHeader.textContent = quiz[num].question;
+    const answers = quiz[num].answers;
+    document.getElementById('answer1').innerHTML = "1. " + answers[0]
+    document.getElementById('answer2').innerHTML = "2. " + answers[1]
+    document.getElementById('answer3').innerHTML = "3. " + answers[2]
+    document.getElementById('answer4').innerHTML = "4. " + answers[3]
+    document.getElementById('answer5').innerHTML = "5. " + answers[4]
+}
+// Adding hidden element
+document.getElementById('start-button').onclick = function () {
+    questions.hidden = false;
+    document.getElementById('starting-section').hidden = true;
+    setQuestion(0);
+    timerInterval = setInterval(function () {
+        timer.innerHTML = --seconds;
+    }, 1000);
+}
+
+function showTemporaryMessage(type){
+    if(type === "correct"){
+        document.getElementById('correct').hidden = false;
+        document.getElementById('wrong').hidden = true;
+    } else {
+        document.getElementById('correct').hidden = true;
+        document.getElementById('wrong').hidden = false;
+    }
+    document.getElementById('temp-results').hidden = false; 
+    temporaryMessageTimeout = setTimeout(function () {
+        document.getElementById('temp-results').hidden = true;
+    }, 2000)
+}
+//Function for checking answers
+function answer(userAnswer) {
+    clearTimeout(temporaryMessageTimeout);
+    if (userAnswer === quiz[questionNumber].correctAnswer) {
+        showTemporaryMessage("Correct")
+    } else {
+        seconds -= 20;
+        timer.innerHTML = seconds; 
+        showTemporaryMessage("Wrong")
+    }
+
+    question += 1;
+    if (question < quiz.length) {
+        setQuestion(questionNumber);
+    } else {
+        quizHeader.innerHTML = "All done";
+        questions.hidden = true;
+        document.getElementById('quiz-results').hidden = false;
+        clearInterval(timerInterval);
+        if (seconds < 0) {
+            seconds = 0
+        }
+        document.getElementById('results').innerHTML = seconds;
+    }
+}
+
+document.getElementById('answer1').onclick = function () { answer(1) };
+document.getElementById('answer2').onclick = function () { answer(2) };
+document.getElementById('answer3').onclick = function () { answer(3) };
+document.getElementById('answer4').onclick = function () { answer(4) };
+document.getElementById('answer4').onclick = function () { answer(5) };
+
+document.getElementById('submit').onclick = function () {
+    var initials = document.getElementById('initials').value;
+    if (!initials || initials.length > 3){
+        showTemporaryMessage("Wrong");
+        return;
+    }
+    initials = initials.toUpperCase();
+    const scores = JSON.parse(localStorage.getItem("scores")) || [];
+    const score = { initials, seconds }
+    scores.push(score);
+    seconds = 0;
+    scores.sort(function (a, b) {
+        return b.seconds - a.seconds
+    });
+    localStorage.setItem("scores", JSON.stringify(scores))
+    window.location.href = "./highscore.html";
+};
+
